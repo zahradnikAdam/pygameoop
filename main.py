209d73259e10fb007b96a6891df0a5c165aefbc3
Adam Stibor <Adam.Stibor@student.souepl.cz>
2026-02-23 13:36:39 +0100

    rewgrd
    rezrez

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from game.game import Game
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.display.set_caption("Kočičárna")
+    pygame.display.set_caption("Rashfordík")
 
     game = Game(screen)
     game.run()
